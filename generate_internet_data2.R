37ef0273043194b5d00206730cce7837e3a51393
michbur <michalburdukiewicz@gmail.com>
2017-05-12 13:40:47 +0200

    added aggregated data

--- generate_internet_data2.R
+++ generate_internet_data2.R
@@ -1,7 +1,6 @@
 library(haven)
 library(dplyr)
 
-
 # variable-weight: http://diagnoza.com/bazy_czytaj.html
 
 load("./Diagnoza_dane-master/osoby.RData")
@@ -20,17 +19,21 @@ samotnosc_pytanie <- grep(pattern = "osamotnion", x = tolower(osobyDict$descript
 internet_zawodowo <- setdiff(grep(pattern = "w celach zawodowych", x = tolower(osobyDict$description)),
                                                    grep(pattern = "kiedykolwiek", x = tolower(osobyDict$description))) + 1
 
-
-
-
 get_data <- function(column_ids, column_name){
   partial_dat <- data.frame(osoby[, column_ids])
-
+  
   res <- do.call(rbind, lapply(colnames(partial_dat), function(ith_colname) {
+
+    # value_vec <- if(class(partial_dat[, ith_colname]) == "labelled") {
+    #   as_factor(partial_dat[, ith_colname])
+    # } else {
+    #   partial_dat[, ith_colname]
+    # }
+     
     data.frame(id = 1L:nrow(osoby),
                value = partial_dat[, ith_colname], 
                rok = substr(ith_colname, 0, 1),
-               plec = osoby[[6]], 
+               plec = as_factor(osoby[[6]]), 
                weight = wagi[, grep(substr(ith_colname, 0, 1), letters) + 1],
                woj =  osoby[[74]],
                region66 = osoby[[77]])
@@ -47,6 +50,7 @@ get_data <- function(column_ids, column_name){
 }
 
 
+tmp <- get_data(internet_zawodowo, "internet_zawodowo")
 
 internet_samotnosc <- inner_join(get_data(internet_pytanie, "internet_godz"),
            get_data(samotnosc_pytanie, "samotnosc")) %>% 
@@ -59,3 +63,55 @@ internet_samotnosc <- inner_join(get_data(internet_pytanie, "internet_godz"),
 # internet_godz: liczba godzin w internecie w ciagu ostatniego tygodnia
 # samotnosc: frakcja osob odczuwajacych samotnosc
 # internet_zawodowo: frakcja osob, ktore wykorzystuja internet w celach zawodowych
+
+partial_dat <- data.frame(osoby[, c(internet_zawodowo, samotnosc_pytanie, internet_pytanie)])
+
+full_survs <- (3 == substr(colnames(partial_dat), 0, 1) %>% 
+  table %>% 
+  as.list %>% 
+  unlist) %>% 
+  which %>% 
+  names 
+
+dat <- lapply(full_survs, function(ith_surv) {
+  cbind(plec = osoby[6], 
+        waga = wagi[, grep(substr(ith_surv, 0, 1), letters) + 1],
+        woj =  osoby[[74]],
+        region66 = osoby[[77]],
+        partial_dat[c(grep(paste0("^", ith_surv), colnames(partial_dat)))]) %>% 
+    filter(apply(., 1, function(i) all(!is.na(i)))) %>%
+    filter(apply(., 1, function(i) all(i > 0))) %>%
+    mutate(plec = as_factor(.[[1]]),
+           waga = .[[2]],
+           woj = as_factor(.[[3]]),
+           region66 = .[[4]],
+           internet_zawodowo = as_factor(.[[5]]),
+           samotnosc = as_factor(.[[6]]),
+           internet_h = as.numeric(as.character(.[[7]]))) %>% 
+    filter(internet_zawodowo %in% c("TAK", "NIE", "TAK zaznaczone", "NIE zaznaczone", 
+                                    "nigdy", "kiedykolwiek", "w ostatnim tygodniu")) %>% 
+    droplevels %>% 
+    select(plec, waga, woj, region66, internet_zawodowo, samotnosc, internet_h) %>%
+    mutate(rok = factor(ith_surv, levels = letters[1L:8], 
+                        labels = c(2000, 2003, 2005, 2007, 2009, 2011, 2013, 2015)),
+           internet_zawodowo = forcats::fct_collapse(internet_zawodowo, 
+                                                     TAK = c("TAK", "TAK zaznaczone", "kiedykolwiek", 
+                                                             "w ostatnim tygodniu"),
+                                                     NIE = c("NIE", "NIE zaznaczone")))
+}) %>% do.call(rbind, .) %>% 
+  group_by(plec, internet_zawodowo, samotnosc) %>% 
+  summarise(internet_h = sum(waga*internet_h)/sum(waga), n = length(waga)) %>% 
+  ungroup %>% 
+  write.csv(file = "internet_samotnosc_aggregated.csv", row.names = FALSE)
+  
+
+
+
+
+ggplot(dat, aes(x = plec, y = internet_h, fill = samotnosc)) +
+  geom_col(position = "dodge") +
+  facet_wrap( ~ internet_zawodowo, labeller = label_both) +
+  scale_x_discrete("Płeć") +
+  scale_y_continuous("Użycie internetu w ostatnim tygodniu [h]") +
+  scale_fill_discrete("Uczucie osamotnienia") +
+  theme_bw()
