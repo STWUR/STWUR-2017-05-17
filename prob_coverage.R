c9ef1155c1c92fe15c967b3eb5dfd271ab04ed75
michbur <michalburdukiewicz@gmail.com>
2017-05-16 22:29:20 +0200

    finished probability coverage

--- prob_coverage.R
+++ prob_coverage.R
@@ -2,18 +2,16 @@ library(dplyr)
 library(binom)
 library(ggplot2)
 
-dat <- binom.confint(1L:100, 700)
+dat <- binom.confint(1L:100, 100)
 
-ggplot(dat, aes(x = x, y = cov, color = method)) +
-  geom_line()
-
-tmp <- do.call(rbind, lapply(1L:100, function(dummy)
+prob_cov <- do.call(rbind, lapply(1L:1000, function(dummy)
   do.call(rbind, lapply(1L:100, function(ith_n_success)
-  data.frame(x = ith_n_success, real_mean = rbinom(1, 1000, prob = ith_n_success/1000)/1000)
+  data.frame(x = ith_n_success, real_mean = rbinom(1, 100, prob = ith_n_success/100)/100)
 )) %>% 
   inner_join(dat))) %>% 
-  mutate(cov = real_mean > lower & real_mean < upper) %>% 
+  mutate(inside = real_mean > lower & real_mean < upper) %>% 
   group_by(x, method) %>% 
-  summarise(cov = mean(cov)) %>% 
-  filter(cov != 1)
+  summarise(cov = mean(inside))
 
+ggplot(prob_cov, aes(x = x, y = cov, color = method)) +
+  geom_line()
